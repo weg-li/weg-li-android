ac2ce936dbefff99948f5490aa90fdd8ab586218
leowroth <32112127+leowroth@users.noreply.github.com>
2020-03-10 13:20:45 +0100

    Added Resource and Status to utils for network calls

---
+++ app/src/main/java/com/github/weg_li_android/utils/Resource.kt
@@ -0,0 +1,18 @@
+package com.github.weg_li_android.utils
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+}
---
+++ app/src/main/java/com/github/weg_li_android/utils/Status.kt
@@ -0,0 +1,7 @@
+package com.github.weg_li_android.utils
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
