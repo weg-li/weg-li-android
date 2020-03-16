3d473fd194d7006f93da7a1ef12525bfc384ffa4
leowroth <32112127+leowroth@users.noreply.github.com>
2020-03-16 16:36:58 +0100

    Added car type spinner

--- app/src/main/java/com/github/weg_li_android/MainActivity.kt
+++ app/src/main/java/com/github/weg_li_android/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.github.weg_li_android
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.car_type_list,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            carTypeSpinner.adapter = adapter
+        }
     }
 }
