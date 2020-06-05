843094924a666e1832c641782d6066d24500b3c1
leowroth <32112127+leowroth@users.noreply.github.com>
2020-06-05 16:52:43 +0200

    Added District data class

---
+++ app/src/main/java/com/github/weg_li_android/data/model/District.kt
@@ -0,0 +1,10 @@
+package com.github.weg_li_android.data.model
+
+data class District(
+    val id: Int,
+    val plz: String,
+    val name: String,
+    val email: String,
+    val license: String
+)
+
