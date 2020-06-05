f4471359f0f4776d8911e17d41803b33e90fea4d
leowroth <32112127+leowroth@users.noreply.github.com>
2020-06-05 16:56:17 +0200

    Changed names to same as API

--- app/src/main/java/com/github/weg_li_android/data/model/District.kt
+++ app/src/main/java/com/github/weg_li_android/data/model/District.kt
@@ -2,8 +2,8 @@ package com.github.weg_li_android.data.model
 
 data class District(
     val id: Int,
-    val plz: String,
+    val zip: String,
     val name: String,
     val email: String,
-    val license: String
+    val prefix: String
 )
