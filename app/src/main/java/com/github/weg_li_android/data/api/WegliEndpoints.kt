c28323285830d90030f1100142a6d509a5ff5d9b
leowroth <32112127+leowroth@users.noreply.github.com>
2020-06-05 23:28:21 +0200

    Added coroutines,

--- app/src/main/java/com/github/weg_li_android/data/api/WegliEndpoints.kt
+++ app/src/main/java/com/github/weg_li_android/data/api/WegliEndpoints.kt
@@ -1,11 +1,11 @@
 package com.github.weg_li_android.data.api
 
 import com.github.weg_li_android.data.model.District
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface WegliEndpoints {
 
     @GET("/districts.json")
-    fun getDistricts(): Call<List<District>>
+    suspend fun getDistricts(): Response<List<District>>
 }
--- app/src/main/java/com/github/weg_li_android/data/repository/Repository.kt
+++ app/src/main/java/com/github/weg_li_android/data/repository/Repository.kt
@@ -2,37 +2,9 @@ package com.github.weg_li_android.data.repository
 
 import com.github.weg_li_android.data.api.ServiceBuilder
 import com.github.weg_li_android.data.api.WegliEndpoints
-import com.github.weg_li_android.data.model.District
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class Repository() {
+    var service = ServiceBuilder.buildService(WegliEndpoints::class.java)
 
-    fun getDistricts(): List<District> {
-        val request = ServiceBuilder.buildService(WegliEndpoints::class.java)
-        val call = request.getDistricts()
-        var districts = listOf<District>()
-
-        call.enqueue(object : Callback<List<District>> {
-            override fun onResponse(
-                call: Call<List<District>>,
-                response: Response<List<District>>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        districts = it
-                    }
-                } else {
-                    TODO("Implement Failstate")
-                }
-            }
-
-            override fun onFailure(call: Call<List<District>>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-        })
-
-        return districts
-    }
+    suspend fun getDistricts() = service.getDistricts()
 }
